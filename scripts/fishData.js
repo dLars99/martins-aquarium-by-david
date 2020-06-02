1c43a32f3d03276b2289fac9a5b28c8d83539ae0
David Larsen <davidplarsen@gmail.com>
2020-06-02 09:25:50 -0500

    Add fishdata script and objects, gitignore

---
+++ scripts/fishData.js
@@ -0,0 +1,41 @@
+// Array of fish objects
+
+const fishCollection = [
+    {
+        name: "Angus",
+        species: "Parrot fish",
+        length: 8,
+        location: "Sea of Google",
+        food: "Popcorn"
+    },
+    {
+        name: "Dopey",
+        species: "Ryukin goldfish",
+        length: 6,
+        location: "Sea of Surprise",
+        food: "Bonito flakes"
+    },
+    {
+        name: "Schnauzer",
+        species: "Unicorn fish",
+        length: 25,
+        location: "Scented Sea",
+        food: "Kelp"
+    }, 
+    {
+        name: "Carole Baskin",
+        species: "Lionfish",
+        length: 12,
+        location: "Sea of Seas",
+        food: "Gazelle"
+    },
+    {
+        name: "Gozer",
+        species: "Warm and Fuzzy Fish",
+        length: 137,
+        location: "Sea of Qo'noS",
+        food: "Sleeping minnows"
+    }
+]
+
+console.log(fishCollection);
